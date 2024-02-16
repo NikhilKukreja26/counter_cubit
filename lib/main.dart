97af18cb800d6bc54972f71c14b240491e037db5
NikhilKukreja26 <kukrejanikhil25@gmail.com>
2024-02-16 22:54:46 +0530

    Counter example using Cubit

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_counter_cubit/cubits/counter/counter_cubit.dart';
+import 'package:my_counter_cubit/other.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'MyCounter Cubit',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: BlocConsumer<CounterCubit, CounterState>(
+          listener: (context, state) {
+            if (state.counter == 3) {
+              showDialog(
+                context: context,
+                builder: (context) =>
+                    AlertDialog(content: Text('counter is ${state.counter}')),
+              );
+            } else if (state.counter == -1) {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => const OtherPage(),
+              ));
+            }
+          },
+          builder: (context, state) {
+            return Text(
+              '${state.counter}',
+              style: const TextStyle(
+                fontSize: 52.0,
+              ),
+            );
+          },
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () => context.read<CounterCubit>().increment(),
+            heroTag: 'increment',
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(width: 10.0),
+          FloatingActionButton(
+            onPressed: () => context.read<CounterCubit>().decrement(),
+            heroTag: 'decrement',
+            child: const Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
+}
